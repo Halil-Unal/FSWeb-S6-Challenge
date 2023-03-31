943b11b272d52aab97098ede243820f4e795bc79
HALİL ÜNAL <125642299+Halil-Unal@users.noreply.github.com>
2023-03-31 19:28:17 +0300

    Finish

---
+++ extra.js
@@ -0,0 +1,71 @@
+/*
+import React from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Karakter from "./components/Karakter";
+
+
+
+
+
+
+const App = () => {
+  // Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
+  const [starData, setstartData] = useState(null);
+  const [querry,setquerry]= useState("");
+  useEffect(() => {
+    // Optionally the request above could also be done as
+    axios
+      .get("https://swapi.dev/api/people/", {
+        params: {
+         
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+        setstartData(response.data);
+        setquerry(response.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+    console.log("sayfam render oldu");
+  }, []);
+ const change =(value)=>{
+  setquerry(value);
+ }
+  return (
+
+    <div className="App">
+  
+      <h1 className="Header">Karakterler</h1>
+      <label>Search</label>
+  <input type='text' onChange={e=>change(e.target.value)}></input>
+
+
+     < h4> Star Wars Major Characters List  </h4>
+
+
+    
+
+
+  <div className='data'>
+      {starData ? starData.map((c) => <Karakter stardata={c} />) : "yükleniyor"}
+      </div>
+    </div>
+   
+  );
+}
+
+
+export default App;
+
+*/
--- src/App.js
+++ src/App.js
@@ -3,32 +3,16 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Karakter from "./components/Karakter";
 
-
-
-
-
-
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [starData, setstartData] = useState([]);
+  const [querry, setquerry] = useState("");
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-  const [starData, setstartData] = useState(null);
-  const [querry,setquerry]= useState("");
   useEffect(() => {
-    // Optionally the request above could also be done as
     axios
-      .get("https://swapi.dev/api/people/", {
-        params: {
-         
-        },
-      })
+      .get("https://swapi.dev/api/people/")
       .then(function (response) {
         console.log(response);
-        setstartData(response.data);
-        setquerry(response.data)
+        setstartData(response.data.results);
       })
       .catch(function (error) {
         console.log(error);
@@ -38,31 +22,34 @@ const App = () => {
       });
     console.log("sayfam render oldu");
   }, []);
- const change =(value)=>{
-  setquerry(value);
- }
-  return (
 
+  const handleChange = (event) => {
+    setquerry(event.target.value);
+  };
+
+  const filteredStarData = starData.filter((character) => {
+    return character.name.toLowerCase().includes(querry.toLowerCase());
+  });
+
+  return (
     <div className="App">
-  
       <h1 className="Header">Karakterler</h1>
       <label>Search</label>
-  <input type='text' onChange={e=>change(e.target.value)}></input>
+      <input type="text" value={querry} onChange={handleChange} />
 
+      <h4>Star Wars Major Characters List</h4>
 
-     < h4> Star Wars Major Characters List  </h4>
-
-
-    
-
-
-  <div className='data'>
-      {starData ? starData.map((c) => <Karakter stardata={c} />) : "yükleniyor"}
+      <div className="data">
+        {filteredStarData.length > 0 ? (
+          filteredStarData.map((character) => (
+            <Karakter key={character.name} stardata={character} />
+          ))
+        ) : (
+          <p>No matching characters found.</p>
+        )}
       </div>
     </div>
-   
   );
-}
-
+};
 
 export default App;

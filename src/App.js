bf449e48403a5fa369abce9d42692e28c53ab424
HALİL ÜNAL <125642299+Halil-Unal@users.noreply.github.com>
2023-03-31 12:40:02 +0300

    nice start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Karakter from "./components/Karakter";
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -7,12 +9,34 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const [starData, setstartData] = useState([]);
+  useEffect(() => {
+    // Optionally the request above could also be done as
+    axios
+      .get("https://swapi.dev/api/people/", {
+        params: {
+         
+        },
+      })
+      .then(function (response) {
+        console.log(response);
+        setstartData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+    console.log("sayfam render oldu");
+  }, []);
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+  <Karakter data={starData}>   </Karakter>
     </div>
   );
 }
 
 export default App;
+
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,57 @@
 // Karakter bileşeniniz buraya gelecek
+
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown,DropdownToggle,DropdownMenu,DropdownItem,UncontrolledDropdown} from 'reactstrap';
+export default function Karakter(props) {
+  const { data} = props;
+ 
+
+  return (
+   
+    <UncontrolledDropdown>
+    <DropdownToggle
+      caret
+      color="dark"
+    >
+      {data[0].name}
+    </DropdownToggle>
+    <DropdownMenu dark>
+      <DropdownItem header>
+      gender: {data[0].gender}
+      </DropdownItem>
+      <DropdownItem>
+      Height:  {data[0].height}
+      </DropdownItem>
+      <DropdownItem text>
+      Mass:  {data[0].mass}
+      </DropdownItem>
+      <DropdownItem disabled>
+      Birth Year:  {data[0].birth_year
+}
+      </DropdownItem>
+      <DropdownItem divider />
+      <DropdownItem>
+      Eye Color:  {data[0].eye_color
+}
+      </DropdownItem>
+      <DropdownItem>
+      Hair Color:  {data[0].hair_color
+}
+      </DropdownItem>
+      <DropdownItem>
+      Skin Color:  {data[0].skin_color
+}
+      </DropdownItem>
+    </DropdownMenu>
+  </UncontrolledDropdown>
+
+
+
+
+
+
+);
+
+
+}
